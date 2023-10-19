b6693a42f6b44423242a1bd368d58647e5d0f14c
Ngô Duy Hiếu <hieu1804bg@gmail.com>
2023-10-19 18:47:53 +0700

    Add more getters and setters.

--- src/main/java/Application/Word.java
+++ src/main/java/Application/Word.java
@@ -24,8 +24,24 @@ public class Word {
     this.explainWord = explainWord;
   }
 
+  public String getTargetWord() {
+    return targetWord;
+  }
+
+  public void setTargetWord(String targetWord) {
+    this.targetWord = targetWord;
+  }
+
+  public String getIpa() {
+    return ipa;
+  }
+
   public void setIpa(String ipa) {
     this.ipa = ipa;
+  }
+
+  public String getExplainWord() {
+    return explainWord;
   }
 
   public void setExplainWord(String explainWord) {
