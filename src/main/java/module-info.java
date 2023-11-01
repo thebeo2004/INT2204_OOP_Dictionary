a50d7a334f07844ad98f14fe1c1e878805291d50
theobeo2004 <thenhuet2004@gmail.com>
2023-11-01 17:48:32 +0700

    Nothing

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module com.example.demo2 {
 
   requires org.controlsfx.controls;
   requires org.kordamp.bootstrapfx.core;
+  requires java.sql;
+  requires freetts;
 
   opens com.example.demo2 to javafx.fxml;
   exports com.example.demo2;
