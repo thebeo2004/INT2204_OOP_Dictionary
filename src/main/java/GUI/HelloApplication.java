d24eb0e98ca4eaeb6ba7f10ed6ebcdd70c8ae9a3
Ngô Duy Hiếu <hieu1804bg@gmail.com>
2023-11-03 10:15:33 +0700

    Initiation of the main dictionary connected to a database.

--- src/main/java/GUI/HelloApplication.java
+++ src/main/java/GUI/HelloApplication.java
@@ -4,10 +4,15 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-
 import java.io.IOException;
 
+import Application.*;
 public class HelloApplication extends Application {
+
+  public static Dictionary dictionary = new Dictionary();
+  public static DatabaseManagement databaseManagement = new DatabaseManagement();
+
+
 
   @Override
   public void start(Stage stage) throws IOException {
@@ -16,6 +21,8 @@ public class HelloApplication extends Application {
     stage.setTitle("Hello!");
     stage.setScene(scene);
     stage.show();
+
+    databaseManagement.loadDictionary(dictionary);
   }
 
   public static void main(String[] args) {
