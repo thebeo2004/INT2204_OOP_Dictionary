ed51f7e7bce2f1c209128699e12c4d6be643f6a9
theobeo2004 <thenhuet2004@gmail.com>
2023-11-05 01:30:03 +0700

    Adding lookUp interface and its Controllers, CSS file.

--- src/main/java/GUI/LookUpController.java
+++ src/main/java/GUI/LookUpController.java
@@ -1,38 +1,97 @@
 package GUI;
 
+import static GUI.HelloApplication.dictionary;
+import static GUI.HelloApplication.dictionaryManagement;
+
+import Application.Word;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
 
 import java.util.List;
+import javafx.scene.text.Text;
 
-public class LookUpController {
+public class LookUpController implements Initializable {
 
-    @FXML
-    private TextField textField;
-    private ListView<String> listView;
+  @FXML
+  private TextArea explainWord;
 
+  @FXML
+  private Label ipa;
 
-    @FXML
-    void prefixSearch(MouseEvent event) {
-        //List<String> list = HelloApplication.databaseManagement.prefixList(HelloApplication.dictionary, textField.getText());
-        //listView.getItems().clear();
-        //listView = new ListView<String>();
-        String[] list = {"a", "b", "c"};
-        listView.getItems().addAll(list);
-        System.out.println(listView.getItems());
-        //System.out.println(list.get(0));
+  @FXML
+  private Label function;
+
+  @FXML
+  private TableColumn<String, String> tableColumn;
+
+  @FXML
+  private TableView<String> tableView;
+
+  @FXML
+  private Label targetWord;
+
+  @FXML
+  private TextField typing;
+
+  @Override
+  public void initialize(URL url, ResourceBundle resourceBundle) {
+    tableColumn.setCellValueFactory(data -> new SimpleStringProperty(data.getValue()));
+  }
+
+  @FXML
+  void search(KeyEvent event) {
+
+    tableView.getItems().clear();
+
+    for(String s : dictionaryManagement.search(dictionary, typing.getText())) {
+      tableView.getItems().add(s);
+    }
+  }
+
+  void show(Word word) {
+    targetWord.setText(word.getTargetWord());
+    ipa.setText(word.getIpa());
+    function.setText(word.getFunction());
+    explainWord.setText(word.getExplainWord());
+  }
+
+  @FXML
+  void show(ActionEvent event) {
+
+    Word word = dictionaryManagement.lookUp(dictionary, typing.getText());
+    targetWord.setText("");
+    ipa.setText("");
+    function.setText("");
+    explainWord.setText("");
+
+    if (word != null) {
+      show(word);
+    } else {
+      targetWord.setText("This word doesn't exist");
     }
 
-    public void initialize() {
-        // Initialize the ListView field.
-        this.listView = new ListView<>();
+    tableView.getItems().clear();
+    typing.clear();
+  }
 
-        // Add items to the ListView.
-        this.listView.getItems().addAll("Item 1", "Item 2", "Item 3");
+  @FXML
+  void choosingFromList(MouseEvent event) {
+    Word word = dictionaryManagement.lookUp(dictionary, tableView.getSelectionModel().getSelectedItem());
+    if (word != null) {
+      show(word);
     }
-
+  }
 
 }
