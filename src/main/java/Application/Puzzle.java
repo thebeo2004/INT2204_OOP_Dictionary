2382fbcf91ebe52fb4a6eb43cdce0529411478b9
theobeo2004 <thenhuet2004@gmail.com>
2023-11-28 21:38:04 +0700

    Adding ipa into puzzle's explain

--- src/main/java/Application/Puzzle.java
+++ src/main/java/Application/Puzzle.java
@@ -19,11 +19,39 @@ public class Puzzle {
     this.y = y;
   }
 
+  private String getIpa(String text) {
+    Word w = databaseManagement.lookUp(dictionary, text);
+    String tmp = w.getIpa();
+    String ans = "";
+    int cnt = 0;
+    for(int i = 0; i < tmp.length(); i++) {
+      if (tmp.charAt(i) == '/') {
+        cnt++;
+      }
+
+      if (cnt > 0) {
+        ans += tmp.charAt(i);
+      }
+
+      if (cnt == 2) {
+        return ans;
+      }
+    }
+
+    return ans;
+  }
+
   public void setText(String text) {
     String temp = text.toLowerCase();
     this.text = text.toUpperCase();
+    this.explain = getIpa(text);
+
+    if (!this.explain.isEmpty()) {
+      this.explain += ", ";
+    }
+
     Thread thread = new Thread(() -> {
-      this.explain = translator.translate(temp, "en", "vi");
+      this.explain += translator.translate(temp, "en", "vi");
     });
     thread.start();
   }
