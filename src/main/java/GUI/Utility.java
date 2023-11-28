3be2c5741236dd88cecc04a880d593cd6e84c87a
theobeo2004 <thenhuet2004@gmail.com>
2023-11-28 17:35:24 +0700

    Adding generate random number method into Utility

--- src/main/java/GUI/Utility.java
+++ src/main/java/GUI/Utility.java
@@ -4,6 +4,7 @@ import Application.*;
 import GoogleBasedFeatures.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class Utility {
   public static Dictionary dictionary = new Dictionary();
@@ -20,4 +21,19 @@ public class Utility {
   public static List<String> searchingHistory = new ArrayList<String>();
 
   public static List<String> acceptedCharacter = new ArrayList<>();
+
+  public static List<Integer> generateNumber(int length) {
+    List<Integer> ans = new ArrayList<>();
+    Random rand = new Random();
+    int num = Math.max(1,rand.nextInt((int)length/2));
+    while (ans.size() < num) {
+      int x = rand.nextInt(length);
+      if(!ans.contains(x)) {
+        ans.add(x);
+      }
+    }
+
+    return ans;
+  }
+
 }
