b4e53ee8f95843d3ecd51bfd8ff4899b795c5581
theobeo2004 <thenhuet2004@gmail.com>
2023-11-29 09:52:32 +0700

    New class for loading and writing game's result

---
+++ src/main/java/Application/GameResult.java
@@ -0,0 +1,29 @@
+package Application;
+
+public class GameResult {
+  private int trophies;
+  private int stars;
+
+  public GameResult() {
+    //read from file
+    //read from file
+  }
+
+  public int getTrophies() {
+    return trophies;
+  }
+
+  public int getStars() {
+    return stars;
+  }
+
+  public void setTrophies(int trophies) {
+    this.trophies = trophies;
+    //write file
+  }
+
+  public void setStars(int stars) {
+    this.stars = stars;
+    //write file
+  }
+}
